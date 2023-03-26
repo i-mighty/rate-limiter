6bdb826730c6c3ebb1169cb7b756c08026d98678
Adegboye Josiah <josadegboye@gmail.com>
2023-03-27 00:33:54 +0100

    redundant files clean up

--- server/src/utils/config.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-  env: process.env.NODE_ENV || 'development',
-  PORT: process.env.PORT || 3000,
-  jwtSecret:
-    process.env.JWT_SECRET || 'abdboabfuubdqwd9397835bubdvszczcsnklcnscsnfmsjf',
-};
-
-export default config;
--- server/src/utils/constants.ts
+++
@@ -1,3 +0,0 @@
-export const errorUrl = '/error';
-export const shortBaseUrl = 'https://pbid.io';
-export const shortUrlAlphabet = `abcdefghijklmnopqrstuvwxyz0123456789`;

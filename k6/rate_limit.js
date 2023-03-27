61f9488959bcaedcfe7e7fcb8294265d58f238c2
Adegboye Josiah <josadegboye@gmail.com>
2023-03-27 10:19:12 +0100

    improved tests for monthly quota

--- k6/rate_limit.js
+++ k6/rate_limit.js
@@ -17,7 +17,8 @@ export const options = {
       timeUnit: "1s", // 1000 iterations per second, i.e. 1000 RPS
       duration: "800s",
       preAllocatedVUs: 1, // how large the initial pool of VUs would be
-      maxVUs: 1, // i
+      maxVUs: 1, //
+      startTime: "10s", // make it wait for the burst test to finish
     },
   },
 };
